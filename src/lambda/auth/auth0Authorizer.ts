e078930ebe9eb378520f304b61ec4267bd0b5b60
Matt Bowen <bowen9284@gmail.com>
2020-05-10 16:44:26 -0400

    utilizing middy for handling secrets and cors headers

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -1,63 +1,65 @@
 import {
-  CustomAuthorizerHandler,
   CustomAuthorizerEvent,
   CustomAuthorizerResult,
 } from 'aws-lambda';
 import 'source-map-support/register';
-import * as AWS from 'aws-sdk';
+import * as middy from 'middy';
+import { secretsManager } from 'middy/middlewares';
 
 import { verify } from 'jsonwebtoken';
 import { JwtToken } from '../../auth/JwtToken';
 
-const secretId = process.env.AUTH_0_SECRET_ID
-const secretField = process.env.AUTH_0_SECRET_FIELD
+const secretId = process.env.AUTH_0_SECRET_ID;
+const secretField = process.env.AUTH_0_SECRET_FIELD;
 
-const client = new AWS.SecretsManager()
+export const handler = middy(
+  async (
+    event: CustomAuthorizerEvent,
+    context
+  ): Promise<CustomAuthorizerResult> => {
+    console.log('here', event);
 
-let cachedSecret: string
+    try {
+      const decodedToken = verifyToken(
+        event.authorizationToken,
+        context.AUTH_0_SECRET_FIELD[secretField]
+      );
+      console.log('User was authorized', decodedToken);
 
-export const handler: CustomAuthorizerHandler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
-  console.log('here', event);
+      return {
+        principalId: decodedToken.sub,
+        policyDocument: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Action: 'execute-api:Invoke',
+              Effect: 'Allow',
+              Resource: '*',
+            },
+          ],
+        },
+      };
+    } catch (e) {
+      console.log('User was not authorized', e.message);
 
-  try {
-    const decodedToken = await verifyToken(event.authorizationToken);
-    console.log('User was authorized', decodedToken);
-
-    return {
-      principalId: decodedToken.sub,
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Allow',
-            Resource: '*',
-          },
-        ],
-      },
-    };
-  } catch (e) {
-    console.log('User was not authorized', e.message);
-
-    return {
-      principalId: 'user',
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Deny',
-            Resource: '*',
-          },
-        ],
-      },
-    };
+      return {
+        principalId: 'user',
+        policyDocument: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Action: 'execute-api:Invoke',
+              Effect: 'Deny',
+              Resource: '*',
+            },
+          ],
+        },
+      };
+    }
   }
-};
+);
 
-async function verifyToken(authHeader: string): Promise<JwtToken> {
+function verifyToken(authHeader: string, secret: string): JwtToken {
   if (!authHeader) {
     throw new Error('No authoirzation header');
   }
@@ -69,20 +71,16 @@ async function verifyToken(authHeader: string): Promise<JwtToken> {
   const split = authHeader.split(' ');
   const token = split[1];
 
-  const secretObject: any = await getSecret()
-  const secret = secretObject[secretField]
-
   return verify(token, secret) as JwtToken;
 }
 
-async function getSecret() {
-  if (cachedSecret) return cachedSecret
-
-  const data = await client.getSecretValue({
-    SecretId: secretId
-  }).promise()
-
-  cachedSecret = data.SecretString
-
-  return JSON.parse(cachedSecret)
-}
+handler.use(
+  secretsManager({
+    cache: true,
+    cacheExpiryInMillis: 60000,
+    throwOnFailedCall: true,
+    secrets: {
+      AUTH0_SECRET: secretId,
+    },
+  })
+);
--- src/lambda/http/createImage.ts
+++ src/lambda/http/createImage.ts
@@ -1,11 +1,13 @@
 import {
-  APIGatewayProxyHandler,
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
 } from 'aws-lambda';
 import 'source-map-support/register';
 import * as AWS from 'aws-sdk';
 import * as uuid from 'uuid';
+
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3({
@@ -17,7 +19,7 @@ const imagesTable = process.env.IMAGES_TABLE;
 const bucketName = process.env.IMAGES_S3_BUCKET;
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION;
 
-export const handler: APIGatewayProxyHandler = async (
+export const handler = middy(async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   console.log('Caller event', event);
@@ -28,9 +30,6 @@ export const handler: APIGatewayProxyHandler = async (
   if (!validGroupId) {
     return {
       statusCode: 404,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
       body: JSON.stringify({
         error: 'Group does not exist',
       }),
@@ -44,57 +43,60 @@ export const handler: APIGatewayProxyHandler = async (
 
   return {
     statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
     body: JSON.stringify({
       nenewItem: newItem,
       uploadUrl: url
     }),
-  };
-
-  async function groupExists(groupId: string) {
-    const result = await docClient
-      .get({
-        TableName: groupsTable,
-        Key: {
-          id: groupId,
-        },
-      })
-      .promise();
-
-    console.log('Get group: ', result);
-    return !!result.Item;
   }
+});
 
-  async function createImage(groupId: string, imageId: string, event: any) {
-    const timestamp = new Date().toISOString();
-    const newImage = JSON.parse(event.body);
-
-    const newItem = {
-      groupId,
-      timestamp,
-      imageId,
-      ...newImage,
-      imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
-    };
-    console.log('Storing new item: ', newItem);
-
-    await docClient
-      .put({
-        TableName: imagesTable,
-        Item: newItem,
-      })
-      .promise();
-
-    return newItem;
-  }
-
- function getUploadUrl(imageId: string) {
-    return s3.getSignedUrl('putObject', {
-        Bucket: bucketName,
-        Key: imageId,
-        Expires: +urlExpiration
+async function groupExists(groupId: string) {
+  const result = await docClient
+    .get({
+      TableName: groupsTable,
+      Key: {
+        id: groupId,
+      },
     })
-  }
-};
+    .promise();
+
+  console.log('Get group: ', result);
+  return !!result.Item;
+}
+
+async function createImage(groupId: string, imageId: string, event: any) {
+  const timestamp = new Date().toISOString();
+  const newImage = JSON.parse(event.body);
+
+  const newItem = {
+    groupId,
+    timestamp,
+    imageId,
+    ...newImage,
+    imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
+  };
+  console.log('Storing new item: ', newItem);
+
+  await docClient
+    .put({
+      TableName: imagesTable,
+      Item: newItem,
+    })
+    .promise();
+
+  return newItem;
+}
+
+function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl('putObject', {
+      Bucket: bucketName,
+      Key: imageId,
+      Expires: +urlExpiration
+  })
+}
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
